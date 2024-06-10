1dbaf65f1a05df7d600e153fbe8ac5d423496362
Md. Al Amin Chowdhury <94056482+Alamin031@users.noreply.github.com>
2024-06-10 15:06:06 +0600

    chore: Update API endpoints in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,23 @@
 import axios from 'axios';
-// import dotenv from 'dotenv';
 
-// dotenv.config();
-
-// Get the API URL from environment variables
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/';
 
 export const registerUser = async (userData) => {
-  const response = await axios.post(`${API_URL}api/register`, userData); // Fixed endpoint
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}api/register`, userData);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to register user');
+  }
 };
 
 export const loginUser = async (userData) => {
-  const response = await axios.post(`${API_URL}api/login`, userData); // Fixed endpoint
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}api/login`, userData);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to login user');
+  }
 };
 
 export const getUserProfile = async () => {
@@ -21,7 +25,7 @@ export const getUserProfile = async () => {
   if (!token) {
     throw new Error('Token not found in local storage');
   }
-  
+
   try {
     const response = await axios.get(`${API_URL}api/users/getUserProfile`, {
       headers: {
